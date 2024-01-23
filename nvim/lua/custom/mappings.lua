20fabbe4fc189873cf611a4fc32690c0ffabd7f3
Abhi Ram <ab23hi32011@gmail.com>
2024-01-24 00:53:07 +0530

    Test

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,37 @@
+local M = {}
+--
+-- -- In order to disable a default keymap, use
+-- M.disabled = {
+--   n = {
+--       ["<leader>h"] = "",
+--       ["<C-a>"] = ""
+--   }
+-- }
+--
+-- -- Your custom mappings
+M.general = {
+  n = {
+    [";"] = {":", "enter command mode", opts = { nowait = true } },
+--
+    ["<leader>rr"] = {
+      function ()
+        vim.cmd("term cargo run")
+      end,
+      "Run Rust Files",
+      { noremap = true, silent = true}  
+    },
+--
+    ["<leader>rp"] = {
+      function ()
+        vim.cmd("term python3 %")        
+      end,
+      "Run Python script",
+      { noremap = true, silent = true}
+    },
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+return M 
