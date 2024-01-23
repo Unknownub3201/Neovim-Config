effa0cd15c4f3151cabea79b811016f885a3de54
Abhi Ram <ab23hi32011@gmail.com>
2024-01-24 00:47:05 +0530

    Testing new config

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -1,12 +1,18 @@
--- local base = require("plugins.configs.lspconfig")
-
-local on_attach = require("plugins.configs.lspconfig").on_attach 
-local capabilities = require("plugins.configs.lspconfig").capabilities 
--- local on_attach = base.on_attach 
--- local capabilities = base.capabilities
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach 
+local capabilities = configs.capabilities 
 
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
+-- local servers = { "bash-language-server", "r-languageserver"}
+
+-- for _, lsp in ipairs(servers) do
+  -- lspconfig[lsp].setup {
+    -- on_attach = on_attach,
+    -- capabilities = capabilities,
+  -- }
+-- end
+
 
 lspconfig.clangd.setup{
   on_attach = function (client, bufnr)
